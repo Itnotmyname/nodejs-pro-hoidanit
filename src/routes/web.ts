fa889d5f840a9ccb97b5ffa902194981b3ea7f52
It's not my name <nguyendinhduong080503@gmail.com>
2025-07-05 19:16:47 +0700

    nodejspro

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -2,13 +2,13 @@ import express, { Express } from 'express';
 import { getCreateUserPage, getHomePage, postCreateUser, postDeleteUser, getViewUser, postUpdateUser } from 'controllers/user.controller';
 import { get } from 'http';
 import { getAdminOrderPage, getAdminProductPage, getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controllers';
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+import fileUploadMiddleware from 'src/middleware/multer';
+
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
     router.get("/", getHomePage);
-    
+
     router.post("/handle-delete-user/:id", postDeleteUser);
     router.get("/handle-view-user/:id", getViewUser);
     router.post("/handle-update-user", postUpdateUser);//Chú ý đường dẫn /handle-update-user phải giống như chỗ dòng 18 của view-user.ejs .Nghĩa là nếu bạn ghi đường dẫn là /handle-update-user/:id thì chỗ dòng 18 của view cũng phải là /handle-update-user/<%= user.id %>
@@ -17,10 +17,8 @@ const webRoutes = (app: Express) => {
     router.get("/admin", getDashboardPage);
     router.get("/admin/user", getAdminUserPage);
     router.get("/admin/create-user", getCreateUserPage);//Lấy data
-    //router.post("/admin/handle-create-user", postCreateUser);
-    router.post("/admin/handle-create-user", upload.single('avatar'),(req,res)=>{
-        res.send("ok");
-    } );
+    router.post("/admin/handle-create-user", fileUploadMiddleware("avatar"), postCreateUser);
+
     router.get("/admin/product", getAdminProductPage);
     router.get("/admin/order", getAdminOrderPage);
 
